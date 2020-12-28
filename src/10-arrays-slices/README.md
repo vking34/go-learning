a9f14ab378648fdbcd187a1643b826919c419547
dev_vuong <lzzvkingzzl@gmail.com>
2020-12-28 16:51:22 +0700

    fix(arrays): add slice note

--- src/10-arrays-slices/README.md
+++ src/10-arrays-slices/README.md
@@ -3,7 +3,7 @@
 
 ## Notes
 
-### Arrays
+### Arrays: [n]T
 - We can ignore the length of the array in the declaration by replacing it with ```...```.
 
 - The size of the array is a part of the type. Hence ```[5]int``` and ```[7]int``` are distinct types. Because of this, arrays cannot be resized. Don't worry about this restriction since slices exist to overcome this.
@@ -13,5 +13,38 @@
 - Length: ```len(a)```
 
 - 
-### Slices
+### Slices: []T
 
+- Two ways to create a slice:
+    - create reference from existing array:
+        ```
+        a := [5]int{76, 77, 78, 79, 80}
+        var b []int = a[1:4]
+        ```
+
+    - create reference from array declaration with length specific
+        ```
+        c := []int{6, 7, 8}
+        ```
+    
+- When new elements are appended to the slice, a new array is created. The elements of the existing array are copied to this new array and a new slice reference for this new array is returned. The capacity of the new slice is now twice that of the old slice.
+
+- the zero value of a slice type is ```nil```.
+
+- the structure type of slice:
+    ```
+    type slice struct {  
+        Length        int
+        Capacity      int
+        ZerothElement *byte
+    }
+    ```
+
+- A slice contains the length, capacity and a pointer to the zeroth element of the array. When a slice is passed to a function, even though it's passed by value, the pointer variable will refer to the same underlying array. Hence when a slice is passed to a function as parameter, __changes made inside the function are visible outside the function too__.
+
+
+- Memory optimisation:
+    - Slices hold a reference to the underlying array. As long as the slice is in memory, the array cannot be garbage collected.
+    - Lets assume that we have a very large array and we are interested in processing only a small part of it. The important thing to be noted here is that the array will still be in memory since the slice references it.
+    - Ways to solve:
+        - the ```copy``` function: 
--- src/10-arrays-slices/main.go
+++ src/10-arrays-slices/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+func subtactOne(numbers []int) {
+	for i := range numbers {
+		numbers[i] -= 2
+	}
+
+}
+
+func getCountries() []string {
+	countries := []string{"USA", "Singapore", "Germany", "India", "Australia"}
+	neededCountries := countries[:len(countries)-2]
+	countriesCpy := make([]string, len(neededCountries))
+	copy(countriesCpy, neededCountries) //copies neededCountries to countriesCpy
+	return countriesCpy
+}
+
 func main() {
 	var a [3]int
 	fmt.Println(a)
@@ -12,6 +27,7 @@ func main() {
 	a2 := [...]int{13, 22}
 	fmt.Println(a2)
 
+	fmt.Println("-----------------------")
 	// arrays are value types
 	countries := [...]string{"USA", "China", "India"}
 	newCountries := countries
@@ -19,6 +35,7 @@ func main() {
 	fmt.Println("old:", countries)
 	fmt.Println("new:", newCountries)
 
+	fmt.Println("-----------------------")
 	// iterating
 	for i := 0; i < len(a1); i++ {
 		fmt.Printf("%d th element of a1 is %d\n", i, a1[i])
@@ -30,4 +47,55 @@ func main() {
 
 	// multi-dimensional array
 
+	fmt.Println("-----------------------")
+	// slices
+	// append
+	cars := []string{"Ferrari", "Honda", "Ford"}
+	fmt.Println("cars:", cars, "has old length", len(cars), "and capacity", cap(cars)) //capacity of cars is 3
+	cars = append(cars, "Toyota")
+	fmt.Println("cars:", cars, "has new length", len(cars), "and capacity", cap(cars)) //capacity of cars is doubled to 6
+
+	fmt.Println("-----------------------")
+	// nil
+	var names []string //zero value of a slice is nil
+	if names == nil {
+		fmt.Println("slice is nil going to append")
+		names = append(names, "John", "Sebastian", "Vinay")
+		fmt.Println("names contents:", names)
+	}
+
+	fmt.Println("-----------------------")
+	// append slice
+	veggies := []string{"potatoes", "tomatoes", "brinjal"}
+	fruits := []string{"oranges", "apples"}
+	food := append(veggies, fruits...)
+	fmt.Println("food:", food)
+	fmt.Printf("food length: %d, capicity: %d\n", len(food), cap(food))
+
+	fmt.Println("-----------------------")
+	// passing a slice via function
+	nos := []int{8, 7, 6}
+	fmt.Println("slice before function call", nos)
+	subtactOne(nos)                               //function modifies the slice
+	fmt.Println("slice after function call", nos) //modifications are visible outside
+
+	fmt.Println("-----------------------")
+	// multidimensional slices
+	pls := [][]string{
+		{"C", "C++"},
+		{"JavaScript"},
+		{"Go", "Rust"},
+	}
+
+	for _, v1 := range pls {
+		for _, v2 := range v1 {
+			fmt.Printf("%s ", v2)
+		}
+		fmt.Print("\n")
+	}
+
+	fmt.Println("-----------------------")
+	// memory optimisation
+	countriesNeeded := getCountries()
+	fmt.Println(countriesNeeded)
 }
